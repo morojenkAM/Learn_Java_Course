6e883352ffb6853bc678e749c227b4ef4274b6f8
Alice <mereuta.alisa@mail.ru>
2024-05-28 09:57:26 +0300

    TotalThemes

---
+++ src/DownUpCasting/Vechicle.java
@@ -0,0 +1,8 @@
+package DownUpCasting;
+
+class  Vechicle {//superclasa
+    void drive(){
+        System.out.println("Driving a Vechicle...");
+    }
+}
+
---
+++ src/TypeCasting/TypeCasting.java
@@ -0,0 +1,18 @@
+package TypeCasting;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        int intvalues = 100;
+
+        //Implicit Type Casting from int to long
+
+        long longValue = intvalues;
+        double doubleValue = longValue;
+        System.out.println(intvalues);
+        System.out.println(longValue);
+        System.out.println(doubleValue);
+    }
+
+
+
+}

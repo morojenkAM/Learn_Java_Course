4ac2ae297d1b313e076921e18e8d50b7c11b4a57
Alice <mereuta.alisa@mail.ru>
2024-06-16 00:24:19 +0300

    program that will iterate over a List in 5 different ways

---
+++ src/StreamsAPI_Lambda/List/Iterator2.java
@@ -0,0 +1,28 @@
+package StreamsAPI_Lambda.List;
+
+import java.util.*;
+public class Iterator2 {
+
+    public static void main(String args[]) {
+
+        List<String> myList = new ArrayList<String>();
+
+
+        myList.add("A");
+        myList.add("B");
+        myList.add("C");
+        myList.add("D");
+
+        // Iterator
+        Iterator<String> it = myList.iterator();
+
+        // Condition check for elements in List
+        // using hasNext() method returning true till
+        // there is single element in a List
+        while (it.hasNext()) {
+
+            // Print all elements of List
+            System.out.println(it.next());
+        }
+    }}
+

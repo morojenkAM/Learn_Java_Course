31c267837e63d68f76faea8f9d8b65c7e8becbde
Alice <mereuta.alisa@mail.ru>
2024-06-15 17:34:25 +0300

    a program to convert a given array into a collection with the asList() method
    (cherry picked from commit ba06ecb59177162f6d61f1dd9b22875fe03a3e44)

---
+++ src/StreamsAPI_Lambda/methodAsList.java
@@ -0,0 +1,23 @@
+package StreamsAPI_Lambda;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class methodAsList {
+    public static void main(String[] args) {
+
+        //array input
+        String flowersArray [] = { "Rose" , "Daisy" , " Jasmine" , "Lily" , "Iris", "Orchid" };
+
+        //printing input elements for comparison
+        System.out.println("Array input; " + Arrays.asList(flowersArray));
+
+        //converting array into Colection
+        //with asList()
+        List<String> flowers = Arrays.asList(flowersArray);
+
+        System.out.println("Converted elements; " + flowers);
+
+        }
+    }
+

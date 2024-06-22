3c2387180b9bcd991b1694381524e244bbee476a
Alice <mereuta.alisa@mail.ru>
2024-06-22 18:22:13 +0300

    program that will iterate over a Map with forEach

--- src/StreamsAPI_Lambda/Map/ForEach.java
+++ src/StreamsAPI_Lambda/Map/ForEach.java
@@ -1,5 +1,18 @@
 package StreamsAPI_Lambda.Map;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+
 public class ForEach {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Map<Integer, String> customers = new HashMap<>();
+        customers.put(1, "Ajay");
+        customers.put(2, "Barkha");
+        customers.put(3, "Cathy");
+
+        System.out.println("Using foreach");
+    customers.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+
 }
